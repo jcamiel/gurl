bf80224303bd939fdea27b18d068fc0793be24e9
jcamiel <jeanchristophe.amiel@gmail.com>
2019-07-11 12:56:44 +0200

    Wip.

--- cmd/gurl/main.go
+++ cmd/gurl/main.go
@@ -4,16 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"gurl/ast"
+	"gurl/print"
 	"gurl/run"
 	"log"
-	_ "net/http/pprof"
 	"os"
-	"runtime"
-	"runtime/pprof"
 )
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 
@@ -23,19 +18,6 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close()
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-
 
 	l := log.New(os.Stderr, "", 0)
 
@@ -53,24 +35,12 @@ func main() {
 			os.Exit(1)
 		}
 
-		//printer := print.NewHTMLPrinter()
+		printer := print.NewTermPrinter()
 		//printer := print.NewJSONPrinter()
 		//printer := print.NewHTMLPrinter()
-		//fmt.Print(printer.Print(hurl))
+		fmt.Print(printer.Print(hurl))
 
 		runner := run.NewHttpRunner()
 		runner.Run(hurl)
-	}
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		defer f.Close()
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
 	}
 }
