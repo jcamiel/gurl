cc28661e7bc3805bbfa7ae6e4096c12ce8505dd1
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2019-07-15 21:53:06 +0200

    Wip.

--- template/render.go
+++ template/render.go
@@ -6,12 +6,17 @@ import (
 	"regexp"
 )
 
+// Render the variable inside the template string. If the template
+// string doesn't contains any tempating, the template string is returned.
+// If there is any undefined variable ({{x}} where x is not a key in variables)
+// and error is returned.
 func Render(template string, variables map[string]string) (string, error) {
 	r := regexp.MustCompile(`\{\{(.*?)\}\}`)
 	s := template
 	for {
 		m := r.FindStringSubmatchIndex(s)
 		if m == nil {
+			// No more template variable to render, we exit and return the current string.
 			break
 		}
 		begin, end := m[2], m[3]
