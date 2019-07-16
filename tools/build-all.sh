391865e54d97d0358201132494491035275db033
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2019-07-16 15:42:37 +0200

    Update build-all.sh

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -1,23 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
-GOOS=darwin
-GOARCH=amd64
-export $GOOS
-export $GOARCH
+env GOOS=darwin GOARCH=amd64 go build -o build/bin/darwin/amd64/gurl cmd/gurl/main.go
 
-go build -o build/bin/darwin/amd64/gurl cmd/gurl/main.go
+env GOOS=linux GOARCH=amd64 go build -o build/bin/linux/amd64/gurl cmd/gurl/main.go
 
-GOOS=linux
-GOARCH=amd64
-export $GOOS
-export $GOARCH
-
-go build -o build/bin/linux/amd64/gurl cmd/gurl/main.go
-
-GOOS=windows
-GOARCH=amd64
-export $GOOS
-export $GOARCH
-
-go build -o build/bin/windows/amd64/gurl.exe cmd/gurl/main.go
+env GOOS=windows GOARCH=amd64 go build -o build/bin/windows/amd64/gurl.exe cmd/gurl/main.go
