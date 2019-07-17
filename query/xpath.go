7b60d5af054a15a5b47da24600b51791f2789e25
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2019-07-17 13:53:27 +0200

    Add xml test suite.

--- query/xpath.go
+++ query/xpath.go
@@ -3,6 +3,7 @@ package query
 import (
 	"bytes"
 	"github.com/antchfx/htmlquery"
+	"github.com/antchfx/xmlquery"
 	"github.com/antchfx/xpath"
 	"golang.org/x/net/html"
 )
@@ -12,11 +13,45 @@ import (
 func EvalXPathHTML(expr string, body []byte) (interface{}, error) {
 	e, err := xpath.Compile(expr)
 	if err != nil {
-		return "", nil
+		return nil, err
 	}
-	node, _ := html.Parse(bytes.NewReader(body))
-	root := htmlquery.FindOne(node, "//html")
+	doc, err := html.Parse(bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	root := htmlquery.FindOne(doc, "//html")
 	nav := htmlquery.CreateXPathNavigator(root)
+	val := e.Evaluate(nav)
+
+	switch v := val.(type) {
+	case *xpath.NodeIterator:
+		items := []string{}
+		for {
+			if v.MoveNext() {
+				items = append(items, v.Current().Value())
+			} else {
+				break
+			}
+		}
+		return items, nil
+	default:
+		return val, nil
+	}
+}
+
+
+// Evaluate returns the result of the expression.
+// The result type of the expression is one of the follow: bool,float64,string,[]string (representing collections)).
+func EvalXPathXML(expr string, body []byte) (interface{}, error) {
+	e, err := xpath.Compile(expr)
+	if err != nil {
+		return nil, err
+	}
+	doc, err := xmlquery.Parse(bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	nav := xmlquery.CreateXPathNavigator(doc)
 	val := e.Evaluate(nav)
 
 	switch v := val.(type) {
--- query/xpath_test.go
+++ query/xpath_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/antchfx/htmlquery"
 	"github.com/antchfx/xpath"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
@@ -120,12 +121,13 @@ func check(e error) {
 	}
 }
 
-func TestEvalXPathAll(t *testing.T) {
+func TestEvalXPathHTMLSuite(t *testing.T) {
 
 	fhtlml, _ := os.Open("testdata/sample1.html")
 	defer fhtlml.Close()
-	doc, _ := htmlquery.Parse(bufio.NewReader(fhtlml))
-	nav := htmlquery.CreateXPathNavigator(doc)
+
+	doc, err := ioutil.ReadAll(fhtlml)
+	check(err)
 
 	fcsv, _ := os.Open("testdata/sample1.csv")
 	defer fcsv.Close()
@@ -135,9 +137,53 @@ func TestEvalXPathAll(t *testing.T) {
 		col := strings.Split(s.Text(), "|")
 		test := strings.TrimSpace(col[0])
 		expected := strings.TrimSpace(col[1])
-		expr, err := xpath.Compile(test)
-		check(err)
-		actual := expr.Evaluate(nav)
+		actual, err := EvalXPathHTML(test, []byte(doc))
+		assert.Nil(t, err, test)
+
+		if expected == "true" {
+			v, ok := actual.(bool)
+			assert.True(t, ok, test)
+			assert.True(t, v, test)
+		} else if expected == "false" {
+			v, ok := actual.(bool)
+			assert.True(t, ok, test)
+			assert.False(t, v, test)
+		} else if strings.HasPrefix(expected, "\"") {
+			v, ok := actual.(string)
+			assert.True(t, ok, test)
+			var exp string
+			err := json.Unmarshal([]byte(expected), &exp)
+			check(err)
+			assert.Equal(t, exp, v, test)
+		} else {
+			v, ok := actual.(float64)
+			assert.True(t, ok, test)
+			exp, err := strconv.ParseFloat(expected, 64)
+			check(err)
+			assert.Equal(t, exp, v, test)
+		}
+	}
+	check(s.Err())
+}
+
+func TestEvalXPathXMLSuite(t *testing.T) {
+
+	fhtlml, _ := os.Open("testdata/sample2.xml")
+	defer fhtlml.Close()
+
+	doc, err := ioutil.ReadAll(fhtlml)
+	check(err)
+
+	fcsv, _ := os.Open("testdata/sample2.csv")
+	defer fcsv.Close()
+
+	s := bufio.NewScanner(fcsv)
+	for s.Scan() {
+		col := strings.Split(s.Text(), "|")
+		test := strings.TrimSpace(col[0])
+		expected := strings.TrimSpace(col[1])
+		actual, err := EvalXPathXML(test, []byte(doc))
+		assert.Nil(t, err, test)
 
 		if expected == "true" {
 			v, ok := actual.(bool)
