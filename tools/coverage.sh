335aa48f345e2a941c762bfb1a15064faf3068d4
Jean-Christophe Amiel <jeanchristophe.amiel@orange.com>
2019-07-31 13:17:06 +0200

    Maj job coverage.

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -4,4 +4,4 @@ go test -covermode=count -coverprofile=out/coverage.out ./...
 
 go tool cover -html=out/coverage.out -o out/coverage.html
 
-go tool cover -func=out/coverage.out | grep total: | tr -s '\t' | cut -f 3
+go tool cover -func=out/coverage.out | grep total:
